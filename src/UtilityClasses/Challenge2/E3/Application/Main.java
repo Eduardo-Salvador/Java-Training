f7aba3a09c3c48c24f7d334c0c7d31f74b26de97
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2025-10-12 22:44:04 -0300

    feat: Utility Classes: Challenge 2 - 3 of 4: Customer Registration with Consumption System

---
+++ src/UtilityClasses/Challenge2/E3/Application/Main.java
@@ -0,0 +1,9 @@
+package UtilityClasses.Challenge2.E3.Application;
+import UtilityClasses.Challenge2.E3.Controller.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.menu();
+    }
+}
