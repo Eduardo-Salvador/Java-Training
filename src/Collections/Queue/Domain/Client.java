daff1858255d96bb85ee23a67601a5519e4600f8
Eduardo Salvador <eduardo.salvador.gs@gmail.com>
2025-11-11 12:24:29 -0300

    Training Collections: 11 - Queue

---
+++ src/Collections/Queue/Domain/Client.java
@@ -0,0 +1,18 @@
+package Collections.Queue.Domain;
+
+public class Client {
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client: " + getName();
+    }
+}
