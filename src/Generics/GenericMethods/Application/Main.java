04625bad70a6dd8de4f5724f995ce855142b5b2e
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2025-11-12 16:07:36 -0300

    Training Generics 3: Generic Methods 2/2

--- src/Generics/GenericMethods/Application/Main.java
+++ src/Generics/GenericMethods/Application/Main.java
@@ -1,11 +1,50 @@
 package Generics.GenericMethods.Application;
-
 import Generics.GenericMethods.Controller.GenericMethods;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        GenericMethods.printArray(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        GenericMethods.printArray(new Integer[]{1, 2, 3, 4, 5, 6});
         System.out.println("----------------");
         GenericMethods.printArray(new String[]{"Banana", "Apple", "Strawberry"});
+        System.out.println("----------------");
+        GenericMethods.printArray(new Double[]{5D, 3.2, 3.1425, 12.32, 100D});
+        System.out.println("----------------");
+
+        System.out.println("Max value is: " + GenericMethods.maxValue(10, 20, 30));
+        System.out.println("Max value is: " + GenericMethods.maxValue(10D, 45.2, 33D));
+        System.out.println("Max value is: " + GenericMethods.maxValue("Ana", "Carlos", "Heitor"));
+        System.out.println("----------------");
+
+        List<String> list = new ArrayList<>(List.of("Eduardo", "Joao"));
+        GenericMethods.containsElements(list, "Joao");
+        System.out.println("----------------");
+        List<Double> listD = new ArrayList<>(List.of(2D, 5.43, 3.1415, 82D));
+        GenericMethods.containsElements(listD, 5D);
+        System.out.println("----------------");
+        List<Integer> listI = new ArrayList<>(List.of(3,4,5,12,32,23));
+        GenericMethods.containsElements(listI, 12);
+        System.out.println("----------------");
+
+        GenericMethods.swap(new Integer[]{1, 2, 3, 4, 5, 6}, 2, 5);
+        System.out.println("----------------");
+        GenericMethods.swap(new Double[]{5D, 3.2, 3.1425, 12.32, 100D}, 5D, 12.32);
+        System.out.println("----------------");
+        GenericMethods.swap(new String[]{"Banana", "Apple", "Strawberry"}, "Banana", "Strawberry");
+        System.out.println("----------------");
+
+        List<Integer> listI2 = new ArrayList<>();
+        GenericMethods.copyList(listI, listI2);
+        System.out.println("----------------");
+        List<Double> listD2 = new ArrayList<>();
+        GenericMethods.copyList(listD, listD2);
+        System.out.println("----------------");
+        List<String> list2 = new ArrayList<>();
+        GenericMethods.copyList(list, list2);
+        System.out.println("----------------");
+        List<Number> listN = new ArrayList<>();
+        GenericMethods.copyList(listI2, listN);
+        System.out.println("----------------");
     }
 }
--- src/Generics/GenericMethods/Controller/GenericMethods.java
+++ src/Generics/GenericMethods/Controller/GenericMethods.java
@@ -1,4 +1,6 @@
 package Generics.GenericMethods.Controller;
+import java.util.Arrays;
+import java.util.List;
 
 public class GenericMethods {
     public static <T> void printArray(T[] t){
@@ -6,5 +8,52 @@ public class GenericMethods {
         for (Object o : t){
             System.out.println(o);
         }
+    }
+
+    public static <T extends Comparable<T>> T maxValue (T a, T b, T c){
+        T max = a;
+        if (b.compareTo(max) > 0){
+            max = b;
+        }
+        if (c.compareTo(max) > 0){
+            max = c;
+        }
+        return max;
+    }
+
+    public static <T> void swap(T[] array, T e1, T e2){
+        int p1 = -1;
+        int p2 = -1;
+        System.out.println("Original Array: " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(e1)){
+                p1 = i;
+            }
+            if (array[i].equals(e2)){
+                p2 = i;
+            }
+        }
+        if (p1 == -1 || p2 == -1) {
+            System.out.println("One of the elements was not found.");
+            return;
+        }
+        if (!e1.equals(e2)) {
+            array[p1] = e2;
+            array[p2] = e1;
+            System.out.println("Changed Array: " + Arrays.toString(array));
+        }
+    }
+
+    public static <T> void containsElements(List<T> list, T element){
+        if (list.contains(element)){
+            System.out.println("Element: " + element + ", is presents on list");
+            return;
+        }
+        System.out.println("Element is not found");
+    }
+
+    public static <T> void copyList (List<? extends T> listFrom, List<? super T> listTo){
+        listTo.addAll(listFrom);
+        listTo.forEach(System.out::println);
     }
 }
