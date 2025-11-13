6d37da304b5e9b84e88d825364761176565e57e7
Eduardo Salvador <eduardo.salvador.gs@gmail.com>
2025-11-13 11:18:24 -0300

    Training Lambdas: Predicate

---
+++ src/Lambdas/Predicate/Application/Main.java
@@ -0,0 +1,37 @@
+package Lambdas.Predicate.Application;
+import Lambdas.Predicate.Domain.Employee;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("John", 32, 2500D);
+        Employee e2 = new Employee("Ana", 28, 7499.99);
+        Employee e3 = new Employee("Eduardo", 26, 12430D);
+        Employee e4 = new Employee("Paula", 27, 4000D);
+        Employee e5 = new Employee("Nathalia", 27, 5000D);
+        List<Employee> list = new ArrayList<>(List.of(e1, e2, e3, e4, e5));
+
+        System.out.println("Salary > 5000:");
+        Employee.filterEmployee(list, employee -> employee.getSalary() > 5000D);
+        System.out.println("----------------------------");
+
+        System.out.println("Age > 30:");
+        Employee.filterEmployee(list, employee -> employee.getAge() > 30);
+        System.out.println("----------------------------");
+
+        System.out.println("Name starts A:");
+        Employee.filterEmployee(list, employee -> employee.getName().charAt(0) == 'A');
+        System.out.println("----------------------------");
+
+        System.out.println("Name starts E and Salary > 10.000:");
+        Employee.filterEmployee(list, (employee -> employee.getName().charAt(0) == 'E' && employee.getSalary() > 10000));
+        System.out.println("----------------------------");
+
+        Predicate<Employee> highSalary = e -> e.getSalary() > 10000;
+        System.out.println("Salary < 10.000:");
+        Employee.filterEmployee(list, highSalary.negate());
+        System.out.println("----------------------------");
+    }
+}
---
+++ src/Lambdas/Predicate/Domain/Employee.java
@@ -0,0 +1,41 @@
+package Lambdas.Predicate.Domain;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Employee {
+    private String name;
+    private Integer age;
+    private Double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public Employee(String name, Integer age, Double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - Age: " + age + " - Salary: $" + salary;
+    }
+
+    public static void filterEmployee(List<Employee> list, Predicate<Employee> condition){
+        for(Employee e : list){
+            if(condition.test(e)){
+                System.out.println(e);
+            }
+        }
+    }
+}
