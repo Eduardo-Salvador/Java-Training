a8685e5dd1ae2260c56759bd1ccbace9bffb789d
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-03-01 14:06:32 -0300

    feat: Concurrency Threads: CompletableFuture: Streams with Completable Future

---
+++ src/main/java/Threads_CompletableFuture/StreamsWithCompletableFuture/DataPipeline.java
@@ -0,0 +1,44 @@
+package Threads_CompletableFuture.StreamsWithCompletableFuture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class DataPipeline {
+    private static CompletableFuture<Integer> getIntegerList(){
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + " is searching...");
+            List<Integer> integerList = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                integerList.add(ThreadLocalRandom.current().nextInt(-50, 101));
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(400);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(Thread.currentThread().getName() + " return this list: " + integerList);
+            System.out.println(Thread.currentThread().getName() + " is filtering and sum...");
+            return integerList;
+        }).thenApply(list -> list.stream()
+                .filter(x -> x > 0)
+                .map(x -> x * 3)
+                .collect(Collectors.toList())
+        ).thenApply(list -> list.stream()
+                .reduce(0, Integer::sum)
+        ).exceptionally(e -> Integer.valueOf(e.getMessage()));
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        CompletableFuture<Integer> c1 = DataPipeline.getIntegerList();
+        CompletableFuture<Integer> c2 = DataPipeline.getIntegerList();
+        CompletableFuture<Integer> result = c1.thenCombine(c2, Integer::sum);
+        System.out.println("List 1 result: " + c1.join());
+        System.out.println("List 2 result: " + c2.join());
+        System.out.println("Sum parallel list result: " + result.join());
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime) + " ms");
+    }
+}
